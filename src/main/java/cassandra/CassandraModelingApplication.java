d8b40f65587c037529730d9227f50a960a908c92
akshaybagai <bagai.aks.7@gmail.com>
2015-03-11 00:45:04 -0400

    configure spring boot

---
+++ src/main/java/cassandra/CassandraModelingApplication.java
@@ -0,0 +1,12 @@
+package cassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraModelingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CassandraModelingApplication.class, args);
+    }
+}
